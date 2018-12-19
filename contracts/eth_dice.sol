638bee1615220666441dfe2c0cf11b0b621df21f
Jan Paricka <jparicka@gmail.com>
2018-12-19 19:19:35 +0100

    started on solidity script

--- contracts/eth_dice.sol
+++ contracts/eth_dice.sol
@@ -1,3 +1,26 @@
-pragma solidity............
-contract BlockchainDice {
+pragma solidity ^0.5.1;
+
+contract Dice {
+
+    uint public winMultiplier;
+
+    uint public gamesPlayed;
+    uint public gamesWon;
+    
+    event gameStarted(address _contract, uint multiplier)
+    event playerWins(address _contract, address _winner, uint _win_amount);
+
+    constructor(uint _winMultiplier) 
+        public
+    {
+        winMultiplier = _winMultiplier
+        gamesPlayed = 0;
+        gamesWon = 0;
+        emit gameStarted(address(this), winMultiplier);
+    }
+
+    // function _bid
+    // function _bid with a fake test=true function
+    // function _payRoyalty
+
 }
