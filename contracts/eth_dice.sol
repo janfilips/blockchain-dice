8d7b9252fc07ead36fc2199aad9175bbfc52ff16
Jan Paricka <jparicka@gmail.com>
2018-12-20 21:12:47 +0100

    added on oraclize

--- contracts/eth_dice.sol
+++ contracts/eth_dice.sol
@@ -1,5 +1,30 @@
 pragma solidity ^0.5.1;
 
+
+import "lib/std.sol";
+import "lib/oraclizeAPI.sol";
+
+contract Contract is named("Contract"), mortal, usingOraclize {
+
+  uint256 public randomInt;
+  event onCallback(string result);
+
+  function Contract() {
+    oraclize_setNetwork(networkID_consensys);
+  }
+
+  function __callback(bytes32 myid, string result) {
+    onCallback(result);
+    if (msg.sender != oraclize_cbAddress()) throw;
+    randomInt = parseInt(result);
+  }
+
+  function update() {
+    oraclize_query("URL", "json(https://api.random.org/json-rpc/1/invoke).result.random.data.0", '{"jsonrpc":"2.0","method":"generateIntegers","params":{"apiKey":"<ENTER API KEY HERE>","n":1,"min":1,"max":6,"replacement":true,"base":10},"id":458}');
+  }
+}
+
+
 contract Dice {
 
     uint public winMultiplier;
@@ -38,7 +63,7 @@ contract Dice {
     {
         // this is a simple oracle function for random.org
         // XXX function to call random.org to pick a random number from 1 to 6
-        uint randomNumber = 6;
+        uint randomNumber = 0;
         return (randomNumber);
     }
 
