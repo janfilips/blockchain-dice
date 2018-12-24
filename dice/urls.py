44f579373b065117c1f4ff98873ce780fcd24237
Jan Paricka <jparicka@gmail.com>
2018-12-24 14:07:40 +0100

    added in devel notes

--- dice/urls.py
+++ dice/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    # XXX /contract to report which contract is in use
+    # XXX /ajax/bet to fill DB records (not sure we really want an async-populating thing at the moment)
 ]
