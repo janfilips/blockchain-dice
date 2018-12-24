927c2c87fb61f793a85b988e59da9fc29282f130
Jan Paricka <jparicka@gmail.com>
2018-12-24 14:34:44 +0100

    added on base settings onto the views

--- dice/views.py
+++ dice/views.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
+import web3
+import django
 import datetime
 import random
-
-import json
+import time
 
 from datetime import timedelta
 from django.utils import timezone
@@ -12,7 +15,8 @@ from django.core import serializers
 from django.http import JsonResponse, HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
-#from dice.models import Bets
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dice.settings")
+django.setup()
 
 from web3 import Web3, Account
 from web3.providers.rpc import HTTPProvider
@@ -22,9 +26,6 @@ w3 = Web3(HTTPProvider(settings.ETHEREUM_PROVIDER))
 import logging
 logger = logging.getLogger(__name__)
 
-from robot_topup_wallets import update_wallet_balances
-
-
 
 def home(request):
     return HttpResponse(settings.ETHEREUM_DICE_CONTRACT)
@@ -32,6 +33,6 @@ def home(request):
 def contract(request):
     return HttpResponse(settings.ETHEREUM_DICE_CONTRACT)
 
-def ajaxBet(request):
+def ajax_bet(request):
     return HttpResponse(settings.ETHEREUM_DICE_CONTRACT)
 
