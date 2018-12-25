d175025988f9b932597b71dd7741d90f346da4f0
Jan Paricka <jparicka@gmail.com>
2018-12-25 17:52:21 +0100

    added in oraclizing function for random.org

--- contracts/eth_dice.sol
+++ contracts/eth_dice.sol
@@ -1,54 +1,29 @@
-pragma solidity ^0.5.2;
+pragma solidity ^0.5.0;
+
 import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
 
 
-contract ExampleContract is usingOraclize {
+contract DieselPrice is usingOraclize {
 
-    string public ETHUSD;
-    event LogConstructorInitiated(string nextStep);
-    event LogPriceUpdated(string price);
-    event LogNewOraclizeQuery(string description);
+    uint betAmount;
 
-    mapping (bytes32 => bool) public pendingQueries;
-
-    function Constructor() public payable {
-        emit LogConstructorInitiated("Constructor was initiated. Call 'updatePrice()' to send the Oraclize Query.");
-    }
-
-    function __callback(bytes32 myid, string memory result) public {
-        if (msg.sender != oraclize_cbAddress()) revert();
-        require (pendingQueries[myid] == true);
-        ETHUSD = result;
-        emit LogPriceUpdated(result);
-        delete pendingQueries[myid]; // This effectively marks the query id as processed.
-    }
-
-    function updatePrice() public payable {
-        
-        // XXX check this out https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=16&format=plain&rnd=new
-        
-        if (oraclize_getPrice("URL") > address(this).balance) {
-            emit LogNewOraclizeQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
-        } else {
-            emit LogNewOraclizeQuery("Oraclize query was sent, standing by for the answer..");
-            bytes32 queryId = oraclize_query("URL", "json(https://api.pro.coinbase.com/products/ETH-USD/ticker).price");
-            pendingQueries[queryId] = true;
-        }
-    }
-}
-
-
-contract Dice is usingOraclize {
-
-    uint public gamesPlayed;
-    uint public gamesWon;
+    uint[] public playerNumbers;
     
+    uint public lastWinningNumber;
+
     event GameStarted(address _contract);
     event PlayerBetAccepted(address _contract, address _player, uint[] _numbers, uint _bet);
-    event DiceRolled(address _contract, address _player, uint _winning_number);
-    event WinningNumber(address _contract, uint _winning_number);
-    event PlayerWins(address _contract, address _winner, uint _winning_number);
-    event Cashout(address _contract, address _winner, uint _winning_number, uint _winning_amount);
+    event RollDice(address _contract, address _player, string _description);
+    event NumberGeneratorCallback(address _contract, address _cbAddress);
+    event WinningNumber(address _contract, uint[] _betNumbers, uint _winningNumber);
+    event PlayerWins(address _contract, address _winner, uint _winningNumber, uint _winningAmount);
+    event Cashout(address _contract, address _winner, uint _winningNumber, uint _winningAmount);
+
+
+    uint public gamesPlayed;
+
+    uint public gamesWon;
+    
 
     constructor() 
         public
@@ -58,60 +33,112 @@ contract Dice is usingOraclize {
         emit GameStarted(address(this));
     }
 
-    function rollDice(uint[] memory betNumbers)
-        public 
-        payable 
-        returns(uint, uint) 
-    {
+
+    function rollDice(uint[] memory betNumbers) public payable {
+        
+        playerNumbers = betNumbers;
+
+        address player = msg.sender;
+        
+        betAmount = msg.value;
+        
+        emit PlayerBetAccepted(address(this), msg.sender, betNumbers, msg.value);
+
+
+        if(betNumbers.length != 6) {
+
+            // Making oraclized query to random.org
+            
+            emit RollDice(address(this), player, "Query to random.org was sent, standing by for the answer..");
+            
+            oraclize_query("URL", "https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=16&format=plain&rnd=new");
+
+
+        } else {
+            
+            // Player bets on every number, we cannot run oraclize service, it's 1-1, player wins.
+
+            msg.sender.transfer(msg.value);
+
+            // The game was played, increase the counter.
+
+            gamesPlayed += 1;
+
+            gamesWon += 1;
+            
+        }
+
+    }
+
+
+    function __callback(bytes32 myid, string memory result) public {
+        
+        // All the action takes place on when we receive a new number from random.org
 
         bool playerWins;
-        emit PlayerBetAccepted(address(this), msg.sender, betNumbers, msg.value);
-    
+        
         uint winningAmount;
-        uint winningNumber = this.numberGenerator();
+    
+        emit NumberGeneratorCallback(address(this), msg.sender);
+    
+        address player = oraclize_cbAddress();
+        require(msg.sender == player);
+        
+        uint winningNumber = parseInt(result);
+        emit WinningNumber(address(this), playerNumbers, winningNumber);
 
-        for (uint i = 0; i < betNumbers.length; i++) {
 
-            uint betNumber = betNumbers[i];
+        for (uint i = 0; i < playerNumbers.length; i++) {
+
+            uint betNumber = playerNumbers[i];
 
             if(betNumber == winningNumber) {
                 playerWins = true;
-                emit PlayerWins(address(this), msg.sender, winningNumber);
 
             }
 
         }
 
         if(playerWins) {
-
-            if(betNumbers.length == 1) {
-                    winningAmount = (msg.value * 588) / 100;
-            }
-            if(betNumbers.length == 2) {
-                    winningAmount = (msg.value * 293) / 100;
-            }
-            if(betNumbers.length == 3) {
-                    winningAmount = (msg.value * 195) / 100;
-            }
-            if(betNumbers.length == 4) {
-                    winningAmount = (msg.value * 142) / 100;
-            }
-            if(betNumbers.length == 5) {
-                    winningAmount = (msg.value * 107) / 100;
-            }
-            if(betNumbers.length == 6) {
-                    winningAmount = msg.value;
-            }
-
-            msg.sender.transfer(winningAmount);
             
-            emit Cashout(address(this), msg.sender, winningNumber, winningAmount);
+            // Calculate how much player wins
+
+            if(playerNumbers.length == 1) {
+                    winningAmount = (betAmount * 589) / 100;
+            }
+            if(playerNumbers.length == 2) {
+                    winningAmount = (betAmount * 293) / 100;
+            }
+            if(playerNumbers.length == 3) {
+                    winningAmount = (betAmount * 195) / 100;
+            }
+            if(playerNumbers.length == 4) {
+                    winningAmount = (betAmount * 142) / 100;
+            }
+            if(playerNumbers.length == 5) {
+                    winningAmount = (betAmount * 107) / 100;
+            }
+            if(playerNumbers.length == 6) {
+                    winningAmount = 0;
+            }
+
+            emit PlayerWins(address(this), msg.sender, winningNumber, winningAmount);
+
+            if(winningAmount!=0) {
+
+                msg.sender.transfer(winningAmount);
+                emit Cashout(address(this), msg.sender, winningNumber, winningAmount);
+            
+            }
+            
             gamesWon += 1;
 
         }
 
         gamesPlayed += 1;
-        return (winningNumber, winningAmount);
+
+        lastWinningNumber = winningNumber;
+
     }
 
     function payRoyalty()
@@ -119,22 +146,16 @@ contract Dice is usingOraclize {
         payable
         returns(bool success)
     {
+        
+        // As everything in life it cost money to provide this service. As an example, each and every query to random.org is paid for, covered by the contract.
+        
         uint royalty = address(this).balance/2;
-        address payable royalty1 = 0x661599a312f340a6450B05690c715f0b827dc570;
-        address payable royalty2 = 0xeacd131110FA9241dEe05ccf3e3635D12f629A3b;
-        royalty1.transfer(royalty/2);
-        royalty2.transfer(royalty/2);
+        address payable trustedParty1 = 0x9Fd6BA4B755eA745cBA6751A0E6aD21c722b6Bc4;
+        address payable trustedParty2 = 0x9Fd6BA4B755eA745cBA6751A0E6aD21c722b6Bc4;
+        trustedParty1.transfer(royalty/2);
+        trustedParty2.transfer(royalty/2);
         return (true);
-    }
-    
-    function numberGenerator()
-        public
-        returns(uint)
-    {
-        // XXX TODO function to call random.org for a random number from 1 to 6
-        uint winningNumber = 7; 
-        emit WinningNumber(address(this), winningNumber);
-        return (winningNumber);
+
     }
 
     function getBlockTimestamp()
@@ -153,5 +174,5 @@ contract Dice is usingOraclize {
         return (address(this).balance);
     }
 
+    
 }
-
