3139320c6952eb40aff5c79160e30daa4fe1396a
Jan Paricka <jparicka@gmail.com>
2018-12-26 14:12:39 +0100

    adjsuted to the latest db

--- dice/models.py
+++ dice/models.py
@@ -15,7 +15,7 @@ class Bets(models.Model):
     winAmount = models.FloatField(default=0)
     tx_hash = models.CharField(max_length=42)
     tx_receipt = models.CharField(max_length=2048, default="")
-    oraclize_tx_hash = models.CharField(max_length=42)
+    oraclize_tx_hash = models.CharField(max_length=1024)
     status = models.BooleanField(default=0)
     is_robot = models.BooleanField(default=0)
     created = models.DateTimeField(auto_now=True)
