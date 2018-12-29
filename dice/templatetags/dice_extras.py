27f870cb94b43a5da52f173b561f8bcbe53a79c9
Jan Paricka <jparicka@gmail.com>
2018-12-29 01:40:08 +0100

    conditional status

--- dice/templatetags/dice_extras.py
+++ dice/templatetags/dice_extras.py
@@ -17,7 +17,13 @@ def dict_get(h, key):
     except:
         return None
 
+
+@register.filter(name="random_int")
+def random_int(a, b=None):
+    if b is None:
+        a, b = 0, a
+    return random.randint(a, b)
+
 @register.filter(name="to_wei")
 def to_wei(value):
     return w3.toWei(value,'ether')
-
