ee7c67855f4e6d1902736bc6a5dfb17e7a6c8f8c
Jan Paricka <jparicka@gmail.com>
2018-12-30 23:32:25 +0100

    decommissioned clock

--- dice/urls.py
+++ dice/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from django.urls import path
 from dice.views import ajax_update_player_wallet
 from dice.views import home, ajax_bet, ajax_my_games, ajax_games
-from dice.views import get_clock, get_game_abi, get_game_contract
+from dice.views import get_game_abi, get_game_contract
 
 urlpatterns = [
     # common views
@@ -17,5 +17,4 @@ urlpatterns = [
     # bytecode, abi, clock
     url(r'^abi$', get_game_abi, name='get_game_abi'),
     url(r'^contract$', get_game_contract, name='get_game_contract'),
-    url(r'^clock$', get_clock, name='get_clock'),
 ]
