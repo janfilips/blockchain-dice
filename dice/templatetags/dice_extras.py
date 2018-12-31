788acc34185d84da532a0cebac8d31022ddfeea2
Jan Paricka <jparicka@gmail.com>
2018-12-31 13:40:45 +0100

    general code cleansing

--- dice/templatetags/dice_extras.py
+++ dice/templatetags/dice_extras.py
@@ -12,19 +12,6 @@ from web3.auto import w3
 
 register = template.Library()
 
-@register.filter(name='dict_get')
-def dict_get(h, key):
-    try:
-        return h[key]
-    except:
-        return None
-
-@register.filter(name="random_int")
-def random_int(a, b=None):
-    if b is None:
-        a, b = 0, a
-    return random.randint(a, b)
-
 
 @register.filter(name="to_wei")
 def to_wei(value):
@@ -42,3 +29,17 @@ def get_list_from_string(input_string):
 def get_list_len(input_string):
     input_list = json.loads(input_string)
     return len(input_list)
+
+
+@register.filter(name='dict_get')
+def dict_get(h, key):
+    try:
+        return h[key]
+    except:
+        return None
+
+@register.filter(name="random_int")
+def random_int(a, b=None):
+    if b is None:
+        a, b = 0, a
+    return random.randint(a, b)
