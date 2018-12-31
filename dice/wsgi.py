5efcbb2f1dae4649c0ca4602f92a0a141f2bfc4d
Jan Paricka <jparicka@gmail.com>
2018-12-31 16:19:59 +0100

    cleaned devel notes

--- dice/wsgi.py
+++ dice/wsgi.py
@@ -1,9 +1,5 @@
 """
 WSGI config for dice project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
