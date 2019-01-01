34c1c0f612ad332b813dda2ea8848f201b7f5b8d
Jan Paricka <jparicka@gmail.com>
2019-01-01 10:59:35 +0100

    adjusted to the latest model

--- dice/models.py
+++ dice/models.py
@@ -6,19 +6,20 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 
+
 class Players(models.Model):
-    address = models.CharField(max_length=42, default="")
+    address = models.CharField(max_length=128, default="")
     session_key = models.CharField(max_length=128)
 
 class Bets(models.Model):
     status = models.BooleanField(default=0)
-    player = models.CharField(max_length=42)
+    player = models.CharField(max_length=128)
     win_number = models.IntegerField(default=0)
-    numbers = models.CharField(max_length=42, default="")
+    numbers = models.CharField(max_length=128, default="")
     amount = models.FloatField(default=0)
     win_amount = models.FloatField(default=0)
-    tx_hash = models.CharField(max_length=42)
-    oracle_query_id = models.CharField(max_length=42, default="")
-    contract = models.CharField(max_length=42)
+    tx_hash = models.CharField(max_length=128)
+    oracle_query_id = models.CharField(max_length=128, default="")
+    contract = models.CharField(max_length=128)
     blocknumber = models.IntegerField(default=0)
     created = models.DateTimeField(auto_now=True)
