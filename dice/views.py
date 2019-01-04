59aa54130fd86ac58f45c91b723164b4cad126e0
Jan Paricka <jparicka@gmail.com>
2019-01-04 21:03:30 +0100

    adjusted notification wording

--- dice/views.py
+++ dice/views.py
@@ -151,7 +151,7 @@ def ajax_notifications(request):
 
         if(recent_event.event_type == "player_wins"):
 
-            notification_text = "Transaction "+str(recent_event.tx_hash)+" wins Ether "+str(recent_event.win_amount)
+            notification_text = "Congratulations your transaction "+str(recent_event.tx_hash)+" wins "+str(recent_event.win_amount)+" Ether."
             recent_event.seen_by_player = True
             recent_event.seen_on = datetime.datetime.now()
             recent_event.save()
