3315c91ea8d8bdcf92ab671278e74f328c4677a5
Jan Paricka <jparicka@gmail.com>
2019-01-04 18:41:42 +0100

    fixed ajax urls

--- dice/urls.py
+++ dice/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     url(r'^abi$', get_game_abi, name='get_game_abi'),
     url(r'^contract$', get_game_contract, name='get_game_contract'),
     # ajax
-    url(r'^ajax/bet$', ajax_bet, name='ajax_bet'),
-    url(r'^ajax/notifications$', ajax_notifications, name='ajax_notifications'),
+    url(r'^ajax/bet/$', ajax_bet, name='ajax_bet'),
+    url(r'^ajax/notifications/$', ajax_notifications, name='ajax_notifications'),
     url(r'^ajax/games/$', ajax_games_html_table, name='ajax_games_html_table'),
-    url(r'^ajax/player/update', ajax_update_player_wallet, name='ajax_update_player_wallet'),
+    url(r'^ajax/player/update/', ajax_update_player_wallet, name='ajax_update_player_wallet'),
 ]
